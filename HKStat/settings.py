c8745b57c09709f11742a3263f7551d398184e3b
mvs80 <36341429+mvs80@users.noreply.github.com>
2018-03-12 16:00:52 +0300

    Add files via upload

--- HKStat/settings.py
+++ HKStat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'st4eirgqb18=czfdddb^q!y(@=a5li#xec37a(uu2&&a3vj5y9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://hotkeyexcel.pythonanywhere.com',]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,16 +74,26 @@ WSGI_APPLICATION = 'HKStat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'HotKeyExcel$hotkeyex_stat',
+#         'USER': 'HotKeyExcel',           # Not used with sqlite3.
+#         'PASSWORD': 'musik1980',       # Not used with sqlite3.
+#         'HOST': 'HotKeyExcel.mysql.pythonanywhere-services.com',          # Set to empty string for localhost. Not used with sqlite3.
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'HotKeyExcel$hotkeyex_stat',
-        'USER': 'HotKeyExcel',           # Not used with sqlite3.
+        'NAME': 'hotkeyex_stat',
+        'USER': 'hotkeyex_stat',           # Not used with sqlite3.
         'PASSWORD': 'musik1980',       # Not used with sqlite3.
-        'HOST': 'HotKeyExcel.mysql.pythonanywhere-services.com',          # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',          # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',               # Set to empty string for default. Not used with sqlite3.
+        'ATOMIC_REQUESTS': True,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
